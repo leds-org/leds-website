c87d8e3a2d9d087546d7117b34e9f2f7d5c13f9a
Tadeu Augusto <tadeuaugusto@yahoo.com.br>
2025-06-18 16:01:07 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,12 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-}
+  output: 'export', // Isso habilita a exportação estática
+  // Opcional: Se o seu site GitHub Pages tiver um subdiretório (ex: username.github.io/seu-repo)
+  // você precisará do basePath e assetPrefix.
+  // Substitua 'seu-repo' pelo nome do seu repositório no GitHub.
+  // basePath: '/seu-repo',
+  // assetPrefix: '/seu-repo/',
+};
 
-export default nextConfig
+export default nextConfig;
