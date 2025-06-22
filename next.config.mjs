d1bee3cb9c64e505e2fb14c0685c2db8973396ad
Tadeu Augusto <tadeuaugusto@yahoo.com.br>
2025-06-22 15:47:42 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/websitev1',
+  assetPrefix: '/websitev1',
   eslint: {
     ignoreDuringBuilds: true,
   },
