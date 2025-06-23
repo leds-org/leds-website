cb810ba8f8a5f5414ef511553502364a403fc1cd
Tadeu Augusto Vilela da Silva <tadeaugusto@yahoo.com.br>
2025-06-23 10:24:27 -0300

    Atualizações

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/leds-website',
-  assetPrefix: '/leds-website',
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -12,6 +9,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-};
+}
 
-export default nextConfig;
+export default nextConfig
